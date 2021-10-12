0734e1769635544e90c61df4bd215b5cfbda9862
Erdélyi Kristóf <erdelyi.kristof@gmail.com>
2021-10-12 15:49:43 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "app"),
+    },
+    host: "0.0.0.0",
+    compress: true,
+    hot: true,
+    port: 8080,
+  },
+  mode: "development",
+  entry: "./app/assets/js/App.js",
+  output: {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(s(a|c)ss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
